f143feeea6868e79f0e48331ca2a97fcdbf00a44
= <=>
2021-02-25 12:38:15 -0300

    removendo test do endereco

--- tests/Feature/EnderecoTest.php
+++ tests/Feature/EnderecoTest.php
@@ -8,28 +8,5 @@ use Tests\TestCase;
 
 class EnderecoTest extends TestCase
 {
-    // public function getToken()
-    // {
-    //     $user = \App\Models\User::factory()->make();
-
-    //     $user->password = bcrypt('1234');
-    //     $user->save();
-
-    //     $response = $this->post('/api/login', [
-    //         'email' => $user->email,
-    //         'password' => '1234'
-    //     ], []);
-
-    //     return $response['access_token'];
-    // }
-
-    /** @test */
-    // public function regras_de_validacao_create()
-    // {
-    //     $user = \App\Models\User::factory()->create();
-
-    //     if(\Auth::user()->endereco->count()) {
-
-    //     }
-    // }
+    
 }
