f070adf1f97a4c565be7d4fae3cf17fa31014af8
= <=>
2021-03-07 13:26:33 -0300

    fiz create

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            \App\Models\TesteSchedule::create('123');
+            \App\Models\TesteSchedule::create(['nome' => '123']);
         })->everyMinute();;
     }
 
