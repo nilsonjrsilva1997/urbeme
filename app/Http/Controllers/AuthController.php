73568246580e7dca1d9f3a661d0278948050aa5b
nilsonjrsilva1997 <nilsonjrsilva1997@gmail.com>
2021-05-13 10:41:36 -0300

    adding field click sign user key in user table

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
 {
@@ -20,7 +21,34 @@ class AuthController extends Controller
         $user = User::create($validatedData);
         $accessToken = $user->createToken('authToken')->accessToken;
 
+        // criando usuário no click sign
+        $signerKey = $this->createUserClickSign($user);
+
+        // adicionando key ao usuário
+        $user->signer_id = $signerKey;
+        $user->save();
+
         return response(['user' => $user, 'access_token' => $accessToken]);
+    }
+
+    public function createUserClickSign($user)
+    {
+        $signatarioData = [
+            "signer" => [
+                "email" => $user->email,
+                "auths" => [
+                    "email"
+                ],
+                "has_documentation" => true,
+                "selfie_enabled" => true,
+                "handwritten_enabled" => true,
+                "official_document_enabled" => true
+            ]
+        ];
+
+        $clickSignUser = Http::post('https://sandbox.clicksign.com/api/v1/signers?access_token=' . env('TOKEN_CLICK_SING'), $signatarioData, []);
+
+        return $clickSignUser['signer']['key'];
     }
 
     public function login(Request $request)
